3e6435226902e67f71edda1ee9138a12faa220f3
JSultonali <jssalijalol@gmail.com>
2021-05-24 01:46:48 +0500

    docs: update version of pg package

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -5,26 +5,28 @@ var jwt = require('jsonwebtoken');
 var User = require('../models/user')();
 
 router.post('/signup', (req, res) => {
-
+    console.log(req.body.user);
+    console.log(bcrypt.hashSync(req.body.user.password, 10));
     User.create({
         full_name: req.body.user.full_name,
         username: req.body.user.username,
         passwordHash: bcrypt.hashSync(req.body.user.password, 10),
         email: req.body.user.email,
     })
-        .then(
-            function signupSuccess(user) {
-                let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
-                res.status(200).json({
-                    user: user,
-                    token: token
-                })
-            },
+    .then(
+        function signupSuccess(user) {
+            console.log(user);
+            let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+            res.status(200).json({
+                user: user,
+                token: token
+            })
+        },
 
-            function signupFail(err) {
-                res.status(500).send(err.message)
-            }
-        )
+        function signupFail(err) {
+            res.status(500).send(err.message)
+        }
+    )
 })
 
 router.post('/signin', (req, res) => {
